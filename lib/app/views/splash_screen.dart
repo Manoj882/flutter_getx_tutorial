aa90e388183333530fe1cf15311de9a86ac61828
Manoj882 <manojbk488@gmail.com>
2024-01-20 19:44:36 +0545

    refactor/ display toast message when press the floating action button

--- lib/app/views/splash_screen.dart
+++ lib/app/views/splash_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_tutorial/app/utils/utils.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Utils.toastMessageCenter("Hello Guys");
+          Utils.toastMessage("How are you?");
+        },
+      ),
+    );
   }
 }
